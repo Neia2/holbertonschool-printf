d7da2a8097747b53eb0ec814f02081721f6d511a
Neia2 <nenaslife@gmail.com>
2024-03-26 13:16:08 +0100

    readme

--- printf_project/my_printf.c
+++ printf_project/my_printf.c
@@ -65,7 +65,9 @@ int print_format(char specifier, va_list ap)
 		count = _putchar(va_arg(ap, int));
 		else if (specifier == 's')
 		count += print_str(va_arg(ap, char *));
-		else if (specifier == 'd' || (specifier == 'i'))
+		else if (specifier == 'd')
+		count += print_digit((long)va_arg(ap, int), 10);
+		else if (specifier == 'i')
 		count += print_digit((long)va_arg(ap, int), 10);
 		else if (specifier == 'x')
 		count +=  print_digit((long)(va_arg(ap, unsigned int)), 16);
@@ -90,6 +92,7 @@ int _printf(const char *format, ...)
 	while (*format != '\0')
 	{
 		if (*format == '%')
+		count += print_format(*(++format), ap);
 		if (*format == 0)
 		count += print_format(*(++format), ap);
 		else
