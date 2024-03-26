765d41a439e3067ca8684e5963e8bcd29bc77b21
Neia2 <nenaslife@gmail.com>
2024-03-26 11:13:25 +0100

    files

--- my_printf.c
+++ my_printf.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * print_str - Prints a string to stdout
@@ -35,20 +36,20 @@ int count = 0;
 char	*symbols = "0123456789abcdef";
 
 if (n < 0)
-{
+   {
 	write(1, "-", 1);
 	return (print_digit(-n, base) + 1);
-}
+   }
 else if (n < base)
-{
+   {
 return (_putchar(symbols[n]));
 
-}
+   }
 else
-{
+   {
 	count = print_digit(n / base, base);
 	return (count + print_digit(n % base, base));
-}
+   }
 }
 /**
  * print_format - Prints formatted output
@@ -62,17 +63,28 @@ int print_format(char specifier, va_list ap)
 	int count = 0;
 
 	if (specifier == 'c')
+   {
 		count = _putchar(va_arg(ap, int));
-		else if (specifier == 's')
-		count += print_str(va_arg(ap, char *));
-		else if (specifier == 'd')
-		count += print_digit((long)va_arg(ap, int), 10);
-		else if (specifier == 'x')
-		count +=  print_digit((long)(va_arg(ap, unsigned int)), 16);
-		else
-		count += write(1, &specifier, 1);
+   }
+   else if (specifier == 's')
+      {
+		char *str = va_arg(ap, char *);
+      count += print_str(str);
+      }
+   else if (specifier == 'd' || specifier == 'i')
+   {
+		count += print_digit(va_arg(ap, long), 10);
+   }
+   else if (specifier == 'x')
+   {
+		count +=  print_digit(va_arg(ap, long), 16);
+   }
+   else
+   {
+	_putchar(specifier);
+		count++;
+   }
 		return (count);
-
 }
 /**
  * _printf - Prints formatted output to stdout
@@ -83,21 +95,24 @@ int print_format(char specifier, va_list ap)
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int count;
+	int count = 0;
 
 	va_start(ap, format);
-	count = 0;
 	while (*format != '\0')
 	{
 		if (*format == '%')
-		count += print_format(*(++format), ap);
+      {
+		   count += print_format(*(++format), ap);
+      }
 		else
-		count += write(1, format, 1);
-		++format;
-	}
+      {
+		   _putchar(*format);
+		   count++;
+	   }
+      ++format;
+}
 	va_end(ap);
 	return (count);
-
 }
 /**
  * main_counter - Test function for _printf
