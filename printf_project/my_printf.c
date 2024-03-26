c8b98e84f5ee86d6c763531ec0e6c6f6982f690d
Neia2 <nenaslife@gmail.com>
2024-03-26 16:49:02 +0100

    update

--- printf_project/my_printf.c
+++ printf_project/my_printf.c
@@ -52,7 +52,7 @@ else
 }
 /**
  * print_format - Prints formatted output
- * @specifier: The format specifier ('c', 's', 'd', 'x')
+ * @specifier: The format specifier ('c', 's', 'd', 'x' 'i')
  * @ap: Variable argument list
  *
  * Return: Number of characters printed
@@ -70,7 +70,7 @@ int print_format(char specifier, va_list *ap)
 		else if (specifier == 'i')
 		count += print_digit((long)va_arg(ap, int), 10);
 		else if (specifier == 'x')
-		count +=  print_digit((long)(va_arg(ap, unsigned int)), 16);
+		count += print_digit((long)va_arg(ap, unsigned int), 16);
 		else
 		count += write(1, &specifier, 1);
 		return (count);
@@ -92,12 +92,15 @@ int _printf(const char *format, ...)
 	while (*format != '\0')
 	{
 		if (*format == '%')
-		count += print_format(*(++format), ap);
-		if (*format == 0)
-		count += print_format(*(++format), ap);
+		{
+		count += print_format(*(++format), &ap);
+		format++;
+		}
 		else
+		{
 		count += write(1, format, 1);
 		++format;
+		}
 	}
 	va_end(ap);
 	return (count);
