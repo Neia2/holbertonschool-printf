45ceaacaae880a8e9a76521aafbab5dc2aaf77d2
Neia2 <nenaslife@gmail.com>
2024-03-26 20:59:07 +0100

    updating code

--- printf_project/my_printf.c
+++ printf_project/my_printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -9,19 +10,19 @@
  *
  * Return: Number of characters printed
  */
-
 int print_str(char *str)
 {
-	int count = 0;
+    int count = 0;
 
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		count++;
-		str++;
-	}
-	return (count);
+    while (*str != '\0')
+    {
+        _putchar(*str);
+        count++;
+        str++;
+    }
+    return count;
 }
+
 /**
  * print_digit - Prints a number in a given base
  * @n: The number to be printed
@@ -31,25 +32,40 @@ int print_str(char *str)
  */
 int print_digit(long n, int base)
 {
-int count = 0;
-char	*symbols = "0123456789abcdef";
+    int count = 0;
+    char *symbols = "0123456789abcdef";
 
-if (n < 0)
-{
-	write(1, "-", 1);
-	return (print_digit(-n, base) + 1);
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        return print_digit(-n, base) + 1;
+    }
+    else if (n < base)
+    {
+        return _putchar(symbols[n]);
+    }
+    else
+    {
+        count = print_digit(n / base, base);
+        return count + print_digit(n % base, base);
+    }
 }
-else if (n < base)
-{
-return (_putchar(symbols[n]));
 
-}
-else
+/**
+ * op_i - Handles 'i' specifier
+ * @val: Variable argument list
+ *
+ * Return: Number of characters printed
+ */
+int op_i(va_list val)
 {
-	count = print_digit(n / base, base);
-	return (count + print_digit(n % base, base));
+    char str;
+
+    str = va_arg(val, int);
+    _putchar(str);
+    return 1;
 }
-}
+
 /**
  * print_format - Prints formatted output
  * @specifier: The format specifier ('c', 's', 'd', 'x' 'i')
@@ -57,23 +73,38 @@ else
  *
  * Return: Number of characters printed
  */
-int print_format(char specifier, va_list *ap)
+int print_format(char specifier, va_list ap)
 {
-	int count = 0;
+    int count = 0;
 
-	if (specifier == 'c')
-		count = _putchar(va_arg(ap, int));
-		else if (specifier == 's')
-		count += print_str(va_arg(ap, char *));
-		else if (specifier == 'd' || 'i')
-		count += print_digit((long)va_arg(ap, int), 10);
-		else if (specifier == 'x')
-		count +=  print_digit((long)(va_arg(ap, unsigned int)), 16);
-		else
-		count += write(1, &specifier, 1);
-		return (count);
+    if (specifier == 'c')
+        count = _putchar(va_arg(ap, int));
+    else if (specifier == 's')
+        count += print_str(va_arg(ap, char *));
+    else if (specifier == 'd' || specifier == 'i')
+        count += print_digit(va_arg(ap, long), 10);
+    else if (specifier == 'x')
+        count += print_digit(va_arg(ap, long), 16);
+    else if (specifier == 'u')
+        count += print_digit(va_arg(ap, unsigned int), 10);
+    else if (specifier == 'o')
+        count += print_digit(va_arg(ap, unsigned int), 8);
+    else if (specifier == 'p')
+    {
+        count += _putchar('0');
+        count += _putchar('x');
+        count += print_digit(va_arg(ap, long), 16);
+    }
+    else if (specifier == '%')
+        count += _putchar('%');
+    else if (specifier == 'i')
+        count += op_i(ap);
+    else
+        count += _putchar('%') + _putchar(specifier);
 
+    return count;
 }
+
 /**
  * _printf - Prints formatted output to stdout
  * @format: The format string
@@ -83,22 +114,30 @@ int print_format(char specifier, va_list *ap)
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int count;
+	int count = 0;
 
-	va_start(ap, format);
-	count = 0;
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		count += print_format(*(++format), ap);
-		else
-		count += write(1, format, 1);
-		++format;
-	}
-	va_end(ap);
-	return (count);
+    if (format == NULL)
+        return -1;
 
+    va_start(ap, format);
+
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            count += print_format(*(++format), ap);
+        }
+        else
+        {
+            count += write(1, format, 1);
+        }
+        ++format;
+    }
+    va_end(ap);
+
+    return count;
 }
+
 /**
  * main_counter - Test function for _printf
  *
@@ -106,10 +145,10 @@ int _printf(const char *format, ...)
  */
 int main_counter(void)
 {
-	int count;
+    int count;
 
-	count = _printf("hello %s\n", "Maria");
-	_printf(" yhe chars written are %d\n", count);
-	count = _printf("The chars are written are %d\n", count);
-return (0);
+    count = _printf("hello %s\n", "Maria");
+    _printf("The chars written are %d\n", count);
+    count = _printf("The chars are written are %d\n", count);
+    return 0;
 }
